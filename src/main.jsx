9b260459e2226d52e800521595fb4027cbc2f332
jemila <abubakarjemiletu08@gmail.com>
2025-09-09 17:19:17 +0100

    app change

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react' 
-import ReactDOM from 'react-dom/client' 
-import App from './App' 
-import './index.css' 
- 
-ReactDOM.createRoot(document.getElementById('root')).render( 
-  <React.StrictMode> 
-    <App /> 
-  </React.StrictMode> 
-) 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter as Router } from 'react-router-dom' // Use HashRouter for GitHub Pages
+import App from './App'
+import { AuthProvider } from './context/AuthContext'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Router>
+  </React.StrictMode>
+)
